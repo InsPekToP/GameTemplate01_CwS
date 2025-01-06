7c287a979db2152fb1bcd9d03752859458a20402
IsPekToP <inspektop1@gmail.com>
2025-01-06 23:14:43 +0100

    закончил js для шапки

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navbarToggler = document.querySelector("[data-nav-toggler]");
+
+navbarToggler.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    this.classList.toggle("active");
+});
+
+
+for (let i= 0; i<navbarLinks.length; i++){
+    navbarLinks[i].addEventListener("click", function(){
+        navbar.classList.toggle("active");
+        navbarToggler.classList.toggle("active");
+    });
+}
